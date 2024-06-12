d06b92702c36a58edb239096927ba1731266ab73
KanchanNaval <116997961+KanchanNaval@users.noreply.github.com>
2024-06-13 02:00:41 +0530

    Add files via upload

---
+++ QR_code/QR_main2.py
@@ -0,0 +1,17 @@
+import qrcode
+from PIL import Image
+import qrcode.constants
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,  # Make sure this is ERROR_CORRECT_H
+    box_size=25,
+    border=4,
+)
+
+qr.add_data("https://www.linkedin.com/in/kanchan-naval/")
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="purple", back_color = "white")
+img.save("linkedin.png")
+
